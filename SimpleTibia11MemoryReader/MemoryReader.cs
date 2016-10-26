eafa1d8745454e4409199b0d0c051071880090a8
Diego jose Reis <diegojosereis@hotmail.com>
2016-10-26 16:50:44 -0200

    initial commit

---
+++ SimpleTibia11MemoryReader/MemoryReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace SimpleMemoryReader
+{
+    public class MemoryReader
+    {
+        [DllImport("kernel32.dll")]
+        public static extern Int32 ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,
+          [In, Out] byte[] buffer, UInt32 size, out IntPtr lpNumberOfBytesRead);
+
+
+
+        public int ReadPointers(IntPtr handle, int baseAddress, params int[] offsets)
+        {
+            IntPtr bytesRead;
+            int currentAddress = baseAddress;
+           
+            byte[] buffer = new byte[4];
+            foreach (int offset in offsets)
+            {
+                ReadProcessMemory(handle, (IntPtr)currentAddress, buffer, 4, out bytesRead);
+                currentAddress = BitConverter.ToInt32(buffer, 0) + offset;
+            }
+            return currentAddress;
+        }
+
+        public short ReadInt16(IntPtr handle, int address)
+        {
+            IntPtr bytesRead;
+            byte[] buffer = new byte[4];
+            ReadProcessMemory(handle, (IntPtr)address, buffer, 4, out bytesRead);
+            return BitConverter.ToInt16(buffer, 0);
+        }
+
+    }
+}
--- SimpleTibia11MemoryReader/Program.cs
+++ SimpleTibia11MemoryReader/Program.cs
@@ -1,15 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimpleTibia11MemoryReader
+namespace SimpleMemoryReader
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //make sure to be logged in otherwise this example will fail.
+
+            //get the tibia process
+            Process tibia = Process.GetProcessesByName("client").First();
+
+            //ty blackd 
+            //adrMyHP="Qt5Core.dll" + 004555C8 > 8 > 1D8 > 60 > 8
+
+            //get the correct module
+            string dllName = "Qt5Core.dll";
+            ProcessModule correctModule = null;
+            for(int i =0;i < tibia.Modules.Count;i++)
+            {
+                if (tibia.Modules[i].ModuleName == dllName)
+                {
+                    correctModule = tibia.Modules[i];
+                    break;
+                }
+            }
+
+            IntPtr moduleHandle = new IntPtr(0);
+            if (correctModule.BaseAddress != null)
+                moduleHandle = correctModule.BaseAddress;
+
+            var baseAddress = moduleHandle + 0x4555c8;       
+            
+            var offsets = new int[] { 0x8, 0x1d8, 0x60, 0x8 };
+
+            MemoryReader reader = new MemoryReader();
+
+            int lifeAddress = reader.ReadPointers(tibia.Handle, (int)baseAddress, offsets);
+
+            int lifeValue = reader.ReadInt16(tibia.Handle, lifeAddress);
+
+            Console.WriteLine($"Address gotten from reader = {lifeAddress.ToString("X")} .");
+            Console.WriteLine($"Value in the Address = {lifeValue}.");
+            Console.ReadLine();
         }
     }
 }
